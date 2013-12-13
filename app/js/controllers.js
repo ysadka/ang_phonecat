edd674eea84a9bba088faeb2573c1548d29dd650
Yaron <ysadka@gmail.com>
2013-12-13 13:34:13 -0800

    step 2 - templates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,16 @@
 'use strict';
 
-/* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope){
+  $scope.phones = [
+    { 'name':'Nexus S',
+      'snippet': 'This is the snippet Nexus S'},
+    { 'name':'Razor',
+      'snippet': 'This is the snippet Razor'},
+    { 'name':'XOOM',
+      'snippet': 'This is the snippet for XOOM'}
+  ]
+
+  $scope.hello = "Hello, World!"
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,6 +2,17 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('PhoneCat controllers', function() {
+  beforeEach(module('phonecatApp'));
 
+  describe('PhoneListCtrl', function(){
+
+    it('should create "phones" model w/ 3 phones', inject(function($controller){
+      var scope = {},
+        ctrl = $controller('PhoneListCtrl', { $scope : scope});
+
+
+      expect(scope.phones.length).toBe(3);
+    }));
+  });
 });
