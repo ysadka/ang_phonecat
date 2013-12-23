f641f8e0d7e3ec36792725dee882172ca77f51e2
Yaron <ysadka@gmail.com>
2013-12-22 19:19:57 -0800

    step 3 - filtering repeaters

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,19 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('PhoneCat App', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('app/index.html');
+  describe('Phone list view', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('Users/yaron/Desktop/Iron.io/angular-phonecat/app/index.html');
+    });
+
+    it('should filter the phone list as user types into the search box', function() {
+      expect(repeater('.phones li').count()).toBe(3);
+
+      input('query').enter('XOOM');
+      expect(repeater('.phones li').count()).toBe(1);
+    });
   });
-
 });
